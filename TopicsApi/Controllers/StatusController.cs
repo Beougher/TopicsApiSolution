7b72f691f058266283f50c1769c37148bdc670b5
ITUStudent <ITUStudent@ML-RefVm-594359>
2022-02-14 12:34:25 -0500

    Add project files.

---
+++ TopicsApi/Controllers/StatusController.cs
@@ -0,0 +1,10 @@
+﻿namespace TopicsApi.Controllers;
+
+public class StatusController:ControllerBase
+{
+    [HttpGet("status/oncalldeveloper")]
+    public ActionResult GetOnCallDeveloper()
+    {
+        return Ok();
+    }
+}
